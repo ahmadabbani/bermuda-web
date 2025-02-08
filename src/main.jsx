fbb15c1cd1dfe5374fc252329bbcee7480b5fcb6
ahmadabbani <ahmdabbanyy@gmail.com>
2025-02-09 00:03:02 +0200

    cookie

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from "react";
+import axios from "axios";
 import { createRoot } from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css"; // Import Bootstrap CSS
 import "bootstrap/dist/js/bootstrap.bundle.min.js"; // Import Bootstrap JS
 import "./App.css";
 import App from "./App.jsx";
-
+// Set default headers before creating root
+axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
